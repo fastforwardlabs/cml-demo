fb7f0a38fdd126c7da18a97d79e45363dbe0e252
mpgreg <mgregory@mgregory-macbookpro.local>
2019-07-19 19:43:20 +0200

    update for rstudio

--- utils/build-engine.sh
+++ utils/build-engine.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 CLUSTER_NAME=`hostname -s | rev | cut -d'-' -f 2- | rev`-
-WORKERS=`echo {5..8}`
-
+WORKERS=`echo {5..6}`
+ENGINE_NAME='spacy'
+ENGINE_FILE={$ENGINE_NAME}.tar
+ENGINE_INSTANCE='latest'
 
 #sudo yum install -y git
 #git clone https://github.com/fastforwardlabs/airline-sentiment
-#cd airline-sentiment
+#cd airline-sentiment/utils
 
-sudo docker build --network=host -t spacy:1 . -f Dockerfile
-sudo docker image save -o ./spacy.tar spacy:1
-sudo chmod 755 ./spacy.tar
+sudo docker build --network=host -t $ENGINE_NAME:$ENGINE_INSTANCE . -f Dockerfile
+sudo docker image save -o ./$ENGINE_FILE $ENGINE_NAME:$ENGINE_INSTANCE
+sudo chmod 755 ./$ENGINE_FILE
 
 for i in $WORKERS
 do
- scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no spacy.tar $CLUSTER_NAME$i:~
- ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $CLUSTER_NAME$i "sudo docker load --input ~/spacy.tar"
+ scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $ENGINE_FILE $CLUSTER_NAME$i:~
+ ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $CLUSTER_NAME$i "sudo docker load --input ~/$ENGINE_FILE"
 done
 
 #Jupyter : /usr/local/bin/jupyter notebook --no-browser --port=$CDSW_APP_PORT --ip=127.0.0.1 --NotebookApp.token='' --NotebookApp.allow_remote_access=True
