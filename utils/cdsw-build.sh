e7b4df41652dfd47689dfcf2778608670cae5f55
mpgreg <milesper@gmail.com>
2019-07-25 14:16:07 +0200

    Update cdsw-build.sh

--- utils/cdsw-build.sh
+++ utils/cdsw-build.sh
@@ -1,6 +1,4 @@
 pip3 install -r /home/cdsw/utils/requirements3.txt
-#pip install -r /home/cdsw/utils/requirements.txt
 python3 -m spacy download en
 
 if [[ ! -d /home/cdsw/R ]]; then mkdir -m 755 /home/cdsw/R; fi
-#Rscript /home/cdsw/utils/install.R
