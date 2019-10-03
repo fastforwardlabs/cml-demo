0af72a903aa8ead31d5a659e9d34681aba19fcc1
trial1101 <trial1101@trycdp.com>
2019-10-03 13:29:49 +0000

    workaround for MLX-975

--- demo0/CDW-test.py
+++ demo0/CDW-test.py
@@ -2,6 +2,16 @@
 # 
 # This example shows how to send SQL queries to Spark.
 
+
+#NOTE: In CDP find the HMS warehouse directory and external table directory by browsing to:
+# Environment -> <env name> ->  Data Lake Cluster -> Cloud Storage
+# copy and paste the external location to the config setting below.
+
+#Temporary workaround for MLX-975
+#In utils/hive-site.xml edit hive.metastore.warehouse.dir and hive.metastore.warehouse.external.dir based on settings in CDP Data Lake -> Cloud Storage
+!cp /home/cdsw/utils/hive-site.xml /etc/hadoop/conf/
+
+
 from __future__ import print_function
 import os
 import sys
@@ -9,15 +19,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import Row, StructField, StructType, StringType, IntegerType
 
 
-#NOTE: In CDP find the HMS external table location by browsing to:
-# Environment -> <env name> ->  Data Lake Cluster -> Cloud Storage
-# copy and paste the external location to the config setting below.
 
 
 spark = SparkSession\
     .builder\
     .appName("PythonSQL")\
-    .config("spark.yarn.access.hadoopFileSystems","s3a://<******BUCKET*****>/<*****DATALAKE*****/*****OBJECT*****")\
+    .config("spark.yarn.access.hadoopFileSystems","s3a://prod-cdptrialuser11-trycdp-com/cdp-lake")\
     .getOrCreate()
 
 spark.sql("SHOW databases").show()
