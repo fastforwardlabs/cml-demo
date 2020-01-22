0e65c8f00f6374d8766aa3ff0330a458bbf545e4
mpgreg <milesper@gmail.com>
2020-01-22 11:07:00 +0100

    added s3gdb location for spark context

--- demo1/TF_image_score.py
+++ demo1/TF_image_score.py
@@ -55,6 +55,7 @@ sc = SparkSession\
     .config("spark.executor.memory", "4g")\
     .config("spark.executor.instances", 2)\
     .config("spark.yarn.access.hadoopFileSystems","s3a://ml-field/demo/")\
+    .config("spark.hadoop.fs.s3a.s3guard.ddb.region", "us-west-2")\
     .getOrCreate()._sc
 
 from IPython.core.display import HTML
