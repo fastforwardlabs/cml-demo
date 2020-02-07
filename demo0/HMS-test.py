fb4b1ecae9bc6f023c2fdded1b7cbd1ed5081f2e
mpgreg <milesper@gmail.com>
2020-02-07 13:10:27 +0100

    public s3

--- demo0/HMS-test.py
+++ demo0/HMS-test.py
@@ -2,12 +2,21 @@
 # 
 # This example shows how to send SQL queries to Spark.
 
-
+# Running from CDP environment S3 bucket
+#---------------------------------------
 #NOTE: In CDP find the HMS warehouse directory and external table directory by browsing to:
 # Environment -> <env name> ->  Data Lake Cluster -> Cloud Storage
-
 #Data taken from http://stat-computing.org/dataexpo/2009/the-data.html
 #!for i in `seq 1987 2008`; do wget http://stat-computing.org/dataexpo/2009/$i.csv.bz2; bunzip2 $i.csv.bz2; sed -i '1d' $i.csv; aws s3 cp $i.csv s3://ml-field/demo/flight-analysis/data/flights_csv/; rm $i.csv; done
+
+#------------------------------------------------
+# New in FEB 2020
+# Running directly from external public S3 bucket
+#------------------------------------------------
+#s3://harshalpatilpublic-s3/flights_csv/ has 2007.csv and 2008.csv 
+#s3://harshalpatilpublic-s3/airports_csv/
+#s3://harshalpatilpublic-s3/airports_csv_extended/
+#we will use this flight dataset for the demo
 
 
 from __future__ import print_function
@@ -16,38 +25,29 @@ import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.types import Row, StructField, StructType, StringType, IntegerType
 
+# spark on kubernetes session
 spark = SparkSession\
     .builder\
     .appName("PythonSQL")\
     .config("spark.executor.memory", "4g")\
     .config("spark.executor.instances", 2)\
-    .config("spark.yarn.access.hadoopFileSystems","s3a://ml-field/demo/flight-analysis/data/")\
+    .config("spark.yarn.access.hadoopFileSystems","s3a://harshalpatilpublic-s3/flights_csv")\
     .config("spark.driver.maxResultSize","4g")\
-    .config("spark.hadoop.fs.s3a.s3guard.ddb.region", "us-west-2")\
+    .config("spark.hadoop.fs.s3a.s3guard.ddb.region", "ap-southeast-1")\
     .getOrCreate()
 
+#create database
+statement = """
+CREATE DATABASE IF NOT EXISTS flight_demo_public
+"""    
+spark.sql(statement)     
+
 spark.sql("SHOW databases").show()
-spark.sql("USE default")
-spark.sql("SHOW tables").show()
 
-#spark.sql("SELECT COUNT(*) FROM `default`.`flights`").show()
-spark.sql("SELECT * FROM `default`.`flights` LIMIT 10").take(5)
-spark.sql("SELECT DepDelay FROM `default`.`flights` WHERE DepDelay > 0.0").take(5)
-
-#spark.sql("SELECT COUNT(*) FROM `default`.`airports`").show()
-spark.sql("SELECT * FROM `default`.`airports` LIMIT 10").show()
-
-
-
-
-
-
-
-
-
-#spark.sql("DROP TABLE IF EXISTS flights").show()
+    
+# create tables    
 statement = '''
-CREATE EXTERNAL TABLE IF NOT EXISTS `default`.`flights` (
+CREATE EXTERNAL TABLE IF NOT EXISTS `flight_demo_public`.`flight_table_public` (
 `Year` int , 
 `Month` int , 
 `DayofMonth` int , 
@@ -79,13 +79,15 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `default`.`flights` (
 `LateAircraftDelay` int )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TextFile 
-LOCATION 's3a://ml-field/demo/flight-analysis/data/flights_csv/'
+LOCATION 's3a://harshalpatilpublic-s3/flights_csv/'
 '''
 spark.sql(statement) 
 
-#spark.sql("DROP TABLE IF EXISTS airports").show()
+spark.sql("USE flight_demo_public")
+spark.sql("SHOW tables").show()
+    
 statement = '''
-CREATE EXTERNAL TABLE IF NOT EXISTS `default`.`airports` (
+CREATE EXTERNAL TABLE IF NOT EXISTS `flight_demo_public`.`airport_table_public` (
 `iata` string , 
 `airport` string ,
 `city` string ,
@@ -95,14 +97,14 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `default`.`airports` (
 `long` double )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TextFile 
-LOCATION 's3a://ml-field/demo/flight-analysis/data/airports_csv/'
+LOCATION 's3a://harshalpatilpublic-s3/airports_csv/'
 '''
 spark.sql(statement) 
 
-
-#spark.sql("DROP TABLE IF EXISTS airports_extended").show()
+spark.sql("SHOW tables").show()
+    
 statement = '''
-CREATE EXTERNAL TABLE IF NOT EXISTS `default`.`airports_extended` (
+CREATE EXTERNAL TABLE IF NOT EXISTS `flight_demo_public`.`airport_extended_public` (
 `ident` string , 
 `type` string ,
 `name` string ,
@@ -117,9 +119,24 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `default`.`airports_extended` (
 `coordinates` string )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TextFile 
-LOCATION 's3a://ml-field/demo/flight-analysis/data/airports-extended/'
+LOCATION 's3a://harshalpatilpublic-s3/airports_extended_csv/'
 '''
-spark.sql(statement) 
-
+spark.sql(statement)     
     
-#spark.stop()
+spark.sql("SHOW tables").show()    
+
+
+#check data counts in these tables
+spark.sql("SELECT count(*) FROM `flight_demo_public`.`flight_table_public`").show()
+
+
+# check data in these tables
+spark.sql("SELECT * FROM `flight_demo_public`.`flight_table_public` LIMIT 10").toPandas()
+spark.sql("SELECT DepDelay FROM `flight_demo_public`.`flight_table_public` WHERE DepDelay > 0.0 LIMIT 10").toPandas()
+spark.sql("SELECT * FROM `flight_demo_public`.`airport_table_public` LIMIT 10").toPandas()
+spark.sql("SELECT * FROM `flight_demo_public`.`airport_extended_public` LIMIT 10").toPandas()
+
+# STOP spark on Kubernetes session
+spark.stop()
+
+
